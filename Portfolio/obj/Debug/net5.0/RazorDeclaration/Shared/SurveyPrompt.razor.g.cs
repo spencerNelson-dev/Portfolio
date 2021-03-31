ea99d74d833ca1b66557e1612edb498895eaf269
spencerNelson-dev <spencernelson144@gmail.com>
2021-03-30 23:34:33 -0600

    added top navigation bar

--- Portfolio/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Portfolio/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace Portfolio.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 1 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 2 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 3 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 4 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 5 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 6 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 7 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 8 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 9 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Portfolio;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\_Imports.razor"
+#line 10 "D:\repos\Portfolio\Portfolio\_Imports.razor"
 using Portfolio.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Portfolio.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\redwa\Desktop\FBlazor\Portfolio\Portfolio\Shared\SurveyPrompt.razor"
+#line 12 "D:\repos\Portfolio\Portfolio\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
